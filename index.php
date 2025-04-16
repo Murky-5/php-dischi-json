fa75ccc59c97a8abad95780d3e324c810d96c849
MirkoDD <mirkodidio111@gmail.com>
2025-04-16 11:43:41 +0200

    mark_up_structure

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista dischi</title>
+</head>
+<body>
+    <div class="header"></div>
+
+    <main>
+        <div class="box">
+            <div class="cover">
+
+            </div>
+            <div class="titolo">
+                <h1>Title</h1>
+            </div>
+            <div class="autore">
+                <h2>Author</h2>
+            </div>
+            <div class="anno">
+                <h3>Year</h3>
+            </div>
+            <div class="genere">
+                <h4>Genre</h4>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
